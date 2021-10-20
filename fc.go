5409c0c35dce27f2ba4646e01a91baac88bce80e
Jamie Carter <jc2820@hotmail.com>
2021-10-20 11:23:05 +0100

    encrypt func encrypts files

--- fc.go
+++ fc.go
@@ -4,21 +4,22 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
-	"encoding/hex"
+	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 )
 
 func main() {
 	filePath := flag.String("f", "./default", "The filepath to the file you want to operate on.")
-	passKey := flag.String("k", "passphrasewhichneedstobe32bytes!", "A key to encrpyt or decrypt your file with...remember this!")
+	passPhrase := flag.String("k", "secret", "A key to encrpyt or decrypt your file with...remember this!")
 	encryptMode := flag.Bool("e", false, "Encrypt Mode: Encrypt the specified file with the key provided.")
 	decryptMode := flag.Bool("d", false, "Decrypt Mode: Attempt to decrypt the file with the key provided.")
 	addMode := flag.Bool("a", false, "Add Mode: Decrypt and append lines given as tail arguments to the file, then reencrypt with the key provided.")
 	flag.Parse()
 
-	fmt.Printf("Working on file at %v with key %v.\n", *filePath, *passKey)
+	fmt.Printf("Working on file at %v\n", *filePath)
 	if len(flag.Args()) > 0 {
 		fmt.Printf("We')ll be adding lines: %s\n", flag.Args())
 	}
@@ -26,23 +27,32 @@ func main() {
 	switch {
 	case *encryptMode && !*decryptMode && !*addMode:
 		fmt.Println("Let's encrpyt!")
-		encrypt(*filePath, *passKey)
+		encrypt(*filePath, *passPhrase)
 	case *decryptMode && !*encryptMode && !*addMode:
 		fmt.Println("Let's decrypt!")
 	case *addMode && !*encryptMode && !*decryptMode:
 		fmt.Println("Add Lines")
 	default:
-		fmt.Println("Please add a single job flag (-e, -d or -a)")
+		fmt.Println("Please add a single job flag (-e, -d or -a). See help -h for more info.")
 	}
 
 }
 
-func encrypt(file, key string) {
-	plainText := []byte(file)
-	block, err := aes.NewCipher([]byte(key))
+func encrypt(file, secret string) {
+	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		fmt.Println(err)
 	}
+	plaintext := []byte(data)
+
+	key32 := sha256.Sum256([]byte(secret))
+	key := key32[:]
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	aesgcm, err := cipher.NewGCM(block)
 	if err != nil {
 		fmt.Println(err)
@@ -53,6 +63,12 @@ func encrypt(file, key string) {
 		fmt.Println(err)
 	}
 
-	cipherText := aesgcm.Seal(nonce, nonce, plainText, nil)
-	fmt.Printf("%x\n", cipherText)
+	ciphertext := aesgcm.Seal(nonce, nonce, plaintext, nil)
+
+	err = ioutil.WriteFile(file, []byte(ciphertext), 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("File encrypted!")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jc2820/filehasher
 
 go 1.17
+
+
